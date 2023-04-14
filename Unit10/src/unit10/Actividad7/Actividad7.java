a519e6ad2e8dfeb75bd80262c4e55266296d4206
Jose Maria <jmcamposdev@gmail.com>
2023-04-14 08:50:34 +0200

    Añadiendo Actividad7

---
+++ Unit10/src/unit10/Actividad7/Actividad7.java
@@ -0,0 +1,30 @@
+package unit10.Actividad7;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+/*
+   Para escribir en un fichero vamos a escribir:
+    "En un lugar de La Mancha,
+    de cuyo nombre no quiero acordarme..."
+
+    La primera Línea caracter a caracter y la segunda en una sola sentencia.
+ */
+public class Actividad7 {
+    public static void main(String[] args) {
+        String cad = "En un lugar de La Mancha,";
+        String cad2 = "de cuyo nombre no quiero acordarme...";
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter("./src/unit10/Actividad7/LaMancha.txt"))) {
+            for (int i = 0; i < cad.length(); i++) { // Caracter a caracter
+                escritor.write(cad.charAt(i));
+            }
+            escritor.newLine(); // Salto de línea
+            escritor.write(cad2); // En una sola sentencia
+            
+            System.out.println("Escritura en el fichero correcta");
+        } catch (Exception e) {
+            System.out.println("No existe el fichero o la ruta es errónea");
+        }
+    }
+}
+
