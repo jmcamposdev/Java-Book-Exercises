17bf5e50a4b4721ec01bb507a61b56064bc02582
Jose Maria <jmcamposdev@gmail.com>
2023-04-14 12:19:59 +0200

    Añadido Actividad21_Aplicacion

---
+++ Unit10/src/unit10/Actividad21_Aplicacion/Actividad21.java
@@ -0,0 +1,134 @@
+package unit10.Actividad21_Aplicacion;
+
+import Functions.GetData;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/*
+    Diseña una pequeña agenda, que muestre el siguiente menú:
+    1. Nuevo contacto.
+    2. Buscar por nombre.
+    3. Mostrar todos.
+    4. Salir.
+    En ella, guardaremos el nombre y el teléfono de un máximo de 20 personas.
+    La opción 1 nos permitirá introducir un nuevo contacto siempre y cuando la agenda no esté llena, comprobando que el nombre no se encuentra insertado ya.
+    La opción 2 muestra todos los teléfonos que coinciden con la cadena que se busca. Por ejemplo, si tecleamos «Pe», mostrará el teléfono de Pedro, de Pepe y de Petunia.
+    La opción 3 mostrará un listado con toda la información (nombres y teléfonos) ordenados alfabéticamente por el nombre.
+    Por último, la opción 4 guarda todos los datos de la agenda en el archivo agenda.ixt.
+    La próxima vez que se ejecute la aplicación, si hay datos guardados, se cargarán en me moria.
+ */
+public class Actividad21 {
+    public static final int OPCION_AÑADIR_CONTACTO = 1;
+    public static final int OPCION_BUSCAR_POR_NOMBRE = 2;
+    public static final int OPCION_MOSTRAR_TODOS = 3;
+    public static final int OPCION_SALIR = 4;
+
+    public static void main(String[] args) {
+        boolean salidaPrograma = false;
+        int userOption;
+        HashMap<String, String> agenda = new HashMap<>();
+        File fileAgenda = new File("./src/unit10/Actividad21_Aplicacion/agenda.txt");
+        boolean ficheroAgendaInaccesible = false;
+        if (!fileAgenda.exists()) {
+            try {
+                fileAgenda.createNewFile();
+            } catch (IOException e) {
+                System.out.println("No se puede crear el fichero agenda.txt es necesario para el programa");
+                ficheroAgendaInaccesible = true;
+            }
+        }
+        
+        if (fileAgenda.exists() && !ficheroAgendaInaccesible) {
+            try(BufferedReader br = new BufferedReader(new FileReader(fileAgenda))) {
+                String cad;
+                String[] values;
+                while ((cad = br.readLine()) != null) {
+                    values = cad.split(" ");
+                    agenda.put(values[0],values[1]);
+                }
+            } catch (IOException e) {
+                System.out.println("No se puede acceder al fichero "+ fileAgenda.getAbsolutePath());
+                ficheroAgendaInaccesible = true;
+            }
+        }
+        
+        if (!ficheroAgendaInaccesible) {
+            while (!salidaPrograma) {
+                System.out.println("""
+                        1. Nuevo contacto
+                        2. Buscar por nombre
+                        3. Mostrar todos
+                        4. Salir""");
+                userOption = GetData.getInt("Ingrese una opción (1-4): ",1,4);
+                switch (userOption) {
+                    case OPCION_AÑADIR_CONTACTO -> {
+                        if (agenda.size() >= 20) {
+                            System.out.println("La agenda se encuentra llena.");
+                        } else {
+                            String name = GetData.getString("Ingrese su Nombre: ");
+                            String telephone = GetData.getString("Ingrese su Telefono: ");
+                            if (agenda.containsKey(name)) {
+                                System.out.println("El nombre insertado ya se encuentra en la agenda.");
+                            } else {
+                                agenda.put(name,telephone);
+                                if (añadirContacto(fileAgenda, name, telephone)) {
+                                    System.out.println("Se ha añadido el contacto Correctamente");
+                                } else {
+                                    System.out.println("No se ha podido añadir el contacto");
+                                }
+                            }
+                        }
+                    }
+                    
+                    case OPCION_BUSCAR_POR_NOMBRE -> {
+                        String busqueda = GetData.getString("Ingrese que desea buscar: ");
+                        if (agenda.isEmpty()) {
+                            System.out.println("No disponemos de contacto actualmente");
+                        } else {
+                            HashMap<String, String> contactosEncontrado = new HashMap<>();
+                            agenda.forEach((key, value) -> {
+                                if (key.startsWith(busqueda)) {
+                                    contactosEncontrado.put(key,value);
+                                }
+                            });
+                            if (contactosEncontrado.isEmpty()) {
+                                System.out.println("No se han encontrado contactos.");
+                            } else {
+                                contactosEncontrado.forEach((key, value) -> System.out.println(key + " " +  value));
+                            }
+                        }
+                    }
+                    
+                    case OPCION_MOSTRAR_TODOS -> {
+                        if (agenda.isEmpty()) {
+                            System.out.println("No disponemos de contacto actualmente");
+                        } else {
+                            TreeMap<String, String> sortedAgenda = new TreeMap<>(agenda);
+                            sortedAgenda.forEach((key, value) -> {
+                                System.out.println(key + " - " + value);
+                            });
+                        }
+                    }
+                    case OPCION_SALIR -> {
+                        salidaPrograma = true;
+                        System.out.println("Todo los valores se han guardado en el fichero.");
+                    }
+                }
+            }
+        }
+    }
+    
+    public static boolean añadirContacto (File fichero, String nombre, String telefono) {
+        boolean contactoAñadido = true;
+        
+        try (PrintWriter pw = new PrintWriter(new FileWriter(fichero , true))) {
+            pw.println(nombre+" "+telefono);
+            pw.flush();
+        } catch (IOException e) {
+            contactoAñadido = false;
+        }
+        return contactoAñadido;
+    }
+}
