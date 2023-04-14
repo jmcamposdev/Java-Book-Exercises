adf704fa840f7aa1c9b85f5bb85d7e699b2910f7
Jose Maria <jmcamposdev@gmail.com>
2023-04-14 15:17:16 +0200

    Creada Actividad25_Aplicacion

---
+++ Unit10/src/unit10/Actividad25_Aplicacion/Actividad25.java
@@ -0,0 +1,17 @@
+package unit10.Actividad25_Aplicacion;
+
+import java.io.File;
+
+/*
+    Repite la Actividad 21 pero guardando los datos en un fichero XML.
+*/
+public class Actividad25 {
+    public static void main(String[] args) {
+        File fileAgenda = new File("./src/unit10/Actividad25_Aplicacion/agenda.xml");
+        if (!fileAgenda.exists()) {
+            System.out.println("No existe el XML de agenda. el Programa no puede seguir.");
+        } else {
+            
+        }
+    }
+}
