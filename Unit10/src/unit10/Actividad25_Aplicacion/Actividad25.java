f1c6a940490637153f9c4aea06560b36d5f9de00
Jose Maria <jmcamposdev@gmail.com>
2023-04-15 22:26:28 +0200

    Comment Some Lines

--- Unit10/src/unit10/Actividad25_Aplicacion/Actividad25.java
+++ Unit10/src/unit10/Actividad25_Aplicacion/Actividad25.java
@@ -7,11 +7,11 @@ import java.io.File;
 */
 public class Actividad25 {
     public static void main(String[] args) {
-        File fileAgenda = new File("./src/unit10/Actividad25_Aplicacion/agenda.xml");
-        if (!fileAgenda.exists()) {
-            System.out.println("No existe el XML de agenda. el Programa no puede seguir.");
-        } else {
-            
-        }
+//        File fileAgenda = new File("./src/unit10/Actividad25_Aplicacion/agenda.xml");
+//        if (!fileAgenda.exists()) {
+//            System.out.println("No existe el XML de agenda. el Programa no puede seguir.");
+//        } else {
+//
+//        }
     }
 }
