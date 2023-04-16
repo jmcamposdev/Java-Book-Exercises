fdbe27dca25ef0992de5075989886c37194b9da3
Jose Maria <jmcamposdev@gmail.com>
2023-04-16 08:52:04 +0200

    Adding Actividad2

---
+++ Unit11/src/unit11/Actividad02/Actividad2.java
@@ -0,0 +1,29 @@
+package unit11.Actividad02;
+
+import java.io.File;
+import java.io.ObjectOutputStream;
+
+/*
+    Escribe como una cadena, en el fichero binario cancionPirata.dat, la siguiente estrofa:
+    Con diez cañones por banda,
+    viento en popa a toda vela,
+    no corta el mar, sino vuela
+    un velero bergantín.
+ */
+public class Actividad2 {
+    public static void main(String[] args) {
+        File archivo = new File("src/unit11/Actividad02/cancionPirata.dat");
+        String estrofa = """
+                Con diez cañones por banda,
+                viento en popa a toda vela,
+                no corta el mar, sino vuela
+                un velero bergantín.""";
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new java.io.FileOutputStream(archivo))) {
+            oos.writeObject(estrofa);
+            System.out.println("Se ha escrito la estrofa en el fichero binario.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
