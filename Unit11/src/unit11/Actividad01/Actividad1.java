1579b9e66e411cfea06dcc4b316107e06fbe72b8
Jose Maria <jmcamposdev@gmail.com>
2023-04-16 08:43:26 +0200

    Adding Actividad1

---
+++ Unit11/src/unit11/Actividad01/Actividad1.java
@@ -0,0 +1,25 @@
+package unit11.Actividad01;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/*
+    Escribir en un archivo datos.dat los valores de una tabla de diez enteros.
+ */
+public class Actividad1 {
+    public static void main(String[] args) {
+        int[] tabla = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        File fichero = new File("src/unit11/Actividad01/datos.dat");
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fichero))) {
+            for (int i : tabla) {
+                oos.writeInt(i);
+            }
+            System.out.println("Datos guardados correctamente");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
