64d8b91c7821dc68c88bf80fe73bbfa6269bcdc6
Jose Maria <jmcamposdev@gmail.com>
2023-04-16 10:59:57 +0200

    Adding Actividad11_Aplicación

---
+++ Unit11/src/unit11/Actividad11_Aplicacion/Actividad11.java
@@ -0,0 +1,21 @@
+package unit11.Actividad11_Aplicacion;
+
+import Functions.GetData;
+
+import java.io.*;
+
+/*
+    Pide un valor double por consola y guárdalo en un archivo binario.
+ */
+public class Actividad11 {
+    public static void main(String[] args) {
+        File archivo = new File("src/unit11/Actividad11_Aplicacion/numeroDouble.dat");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo))) {
+            double numero = GetData.getDouble("Introduce un número real: ",1);
+            oos.writeObject(numero);
+            System.out.println("Se ha escrito el número en el fichero binario.");
+        } catch (IOException e) {
+            System.out.println("Error al escribir el archivo.");
+        }
+    }
+}
