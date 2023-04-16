70cca488113be9a30f77632f19ebada190659223
Jose Maria <jmcamposdev@gmail.com>
2023-04-16 09:01:14 +0200

    Adding Actividad3

---
+++ Unit11/src/unit11/Actividad03/Actividad3.java
@@ -0,0 +1,28 @@
+package unit11.Actividad03;
+
+import Functions.GetData;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+/*
+    Pedir un entero n por consola y, a continuación, pedir n números de tipos double,
+    que iremos insertando un una tabla. Guardar la tabla en un archivo binario.
+ */
+public class Actividad3 {
+    public static void main(String[] args) {
+        File archivo = new File("src/unit11/Actividad03/tableNumeros.dat");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo))) {
+            int n = GetData.getInt("Introduce un número entero: ",1);
+            double[] tabla = new double[n];
+            for (int i = 0; i < n; i++) {
+                tabla[i] = GetData.getDouble("Introduce un número real: ",1);
+            }
+            oos.writeObject(tabla);
+            System.out.println("Se ha escrito la tabla en el fichero binario.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
