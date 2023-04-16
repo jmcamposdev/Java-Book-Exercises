a710265b4866e2b84b6ab5a7f6d9b57f2a25ad15
Jose Maria <jmcamposdev@gmail.com>
2023-04-16 08:52:44 +0200

    Delete Comment

--- Unit11/src/unit11/Unit11.java
+++ Unit11/src/unit11/Unit11.java
@@ -14,7 +14,6 @@ public class Unit11 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
