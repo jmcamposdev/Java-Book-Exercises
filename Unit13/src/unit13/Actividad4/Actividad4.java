c786c247017bb507d078cd1989632e669583d58a
Jose Maria <jmcamposdev@gmail.com>
2023-04-23 15:35:55 +0200

    Added Actividad4

---
+++ Unit13/src/unit13/Actividad4/Actividad4.java
@@ -0,0 +1,33 @@
+package unit13.Actividad4;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+/*
+    Implementar un método estático al que se pasa como parámetro una tabla de tipo genérico y un predicado.
+    El método devuelve otra tabla con los elementos de la tabla original que verifique la condición
+    del predicado. Aplicarlo a una tabla de 50 enteros entre 1 y 100, que devuelva los múltiplos de 3.
+ */
+public class Actividad4 {
+    public static void main(String[] args) {
+        Integer[] enteros = new Integer[50];
+        for (int i = 0; i < 50; i++) {
+            enteros[i] = Math.round((float) Math.random() * 100);
+        }
+        Arrays.sort(enteros);
+        Integer[] filtrada = filtrar(enteros, x -> (x % 3 == 0));
+        System.out.println("Tabla entera: " +  Arrays.toString(enteros));
+        System.out.println("Tabla multiplos de 3: " +  Arrays.toString(filtrada));
+    }
+
+    static <T> T[] filtrar (T[] tabla, Predicate<T> filtro) {
+        T[] filtrada = Arrays.copyOf(tabla, 0);
+        for (T e : tabla) {
+            if (filtro.test(e)) {
+                filtrada = Arrays.copyOf(filtrada, filtrada.length+1);
+                filtrada[filtrada.length - 1] = e;
+            }
+        }
+        return filtrada;
+    }
+}
