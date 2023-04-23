9d282e1638dd35d413880871ebdaef34c101b3ac
Jose Maria <jmcamposdev@gmail.com>
2023-04-23 12:17:42 +0200

    Added Actividad2

---
+++ Unit13/src/unit13/Actividad02/Actividad02.java
@@ -0,0 +1,36 @@
+package unit13.Actividad02;
+
+import unit13.Actividad01.Cliente;
+import unit13.Actividad01.Saludo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Utilizando la interfaz Saludo de la Actividad 1, implementar un método estático que aplique
+    un saludo a un grupo de peorsonas que se le pasa como parámetro en una table. Devolverá
+    los saludos en una lista de cadenas
+ */
+public class Actividad02 {
+    public static void main(String[] args) {
+        Cliente[] clientes = {
+                new Cliente("Paco"),
+                new Cliente("Alvaro"),
+                new Cliente("Sofia"),
+                new Cliente("Calor"),
+                new Cliente("Adrian")
+        };
+        ArrayList<String> saludos = (ArrayList<String>) saludarGrupo(clientes, c -> "Hola " + c.getNombre());
+        for (String saludo : saludos) {
+            System.out.println(saludo);
+        }
+    }
+
+    static <T> List<String> saludarGrupo (T[] grupo, Saludo<T> saludo) {
+        ArrayList<String> saludos = new ArrayList<>();
+        for (T e : grupo) {
+            saludos.add(saludo.saludar(e));
+        }
+        return saludos;
+    }
+}
