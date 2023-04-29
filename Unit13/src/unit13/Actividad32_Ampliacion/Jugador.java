058161f9524bcbc51c7f0bfe8e841aaa339aeb96
Jose Maria <jmcamposdev@gmail.com>
2023-04-29 20:31:55 +0200

    Added Actividad32_Ampliacion

---
+++ Unit13/src/unit13/Actividad32_Ampliacion/Jugador.java
@@ -0,0 +1,51 @@
+package unit13.Actividad32_Ampliacion;
+
+import unit13.Actividad30_Ampliacion.Socio;
+
+public class Jugador {
+    private final String dni;
+    private String nombre;
+    private PosicionCampo posicion;
+    private double estatura;
+
+    public Jugador(String dni, String nombre,  double estatura ,PosicionCampo posicion) {
+        if (!dni.matches(Socio.NIF_PATTERN)) {
+            throw new IllegalArgumentException("El DNI introducido no es válido");
+        }
+        if (nombre.isBlank()) {
+            throw new IllegalArgumentException("El nombre no puede estar vacío");
+        }
+        if (estatura <= 0) {
+            throw new IllegalArgumentException("La estatura no puede ser negativa");
+        }
+
+        this.dni = dni;
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.estatura = estatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public PosicionCampo getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(PosicionCampo posicion) {
+        this.posicion = posicion;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+}
---
+++ Unit13/src/unit13/Actividad32_Ampliacion/PosicionCampo.java
@@ -0,0 +1,5 @@
+package unit13.Actividad32_Ampliacion;
+
+public enum PosicionCampo {
+    PORTERO, DEFENSA, CENTROCAMPISTA, DELANTERO
+}
