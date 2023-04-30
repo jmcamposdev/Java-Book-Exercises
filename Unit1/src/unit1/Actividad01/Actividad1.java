fe1cfd9ce8a872b2877b3c237863e70d0ad6cbb5
Jose Maria <jmcamposdev@gmail.com>
2023-04-30 13:56:53 +0200

    Added Actividad1

---
+++ Unit1/src/unit1/Actividad01/Actividad1.java
@@ -0,0 +1,12 @@
+package unit1.Actividad01;
+
+/*
+    Escribir unas líneas de código que saluden al usuario con el mensaje:
+    "Hola. Encantado de conocerlo."
+ */
+public class Actividad1 {
+    public static void main(String[] args) {
+        System.out.println("Hola. Encantado de conocerlo.");
+
+    }
+}
