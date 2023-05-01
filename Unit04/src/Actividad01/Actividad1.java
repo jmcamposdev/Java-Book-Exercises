513a84c32ce7bd492fd0fe7e605f07399b9cb92b
Jose Maria <jmcamposdev@gmail.com>
2023-05-01 14:02:58 +0200

    Added Actividad1

---
+++ Unit04/src/Actividad01/Actividad1.java
@@ -0,0 +1,25 @@
+package Actividad01;
+
+import java.util.Scanner;
+
+/*
+    Escribe una función eco() a la que se le pasa como parámetro un número n y muestra por pantalla n veces el mensaje "Eco..."
+ */
+public class Actividad1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Pedimos el número de veces que se mostrará el mensaje
+        System.out.print("Introduce el número de veces que se mostrará el mensaje: ");
+        int n = sc.nextInt();
+
+        // Mostramos el mensaje n veces
+        eco(n);
+    }
+
+    private static void eco(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Eco...");
+        }
+    }
+}
