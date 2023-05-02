c87abe1b3e2952c7081c09d58683e1d60b3779a4
Jose Maria <jmcamposdev@gmail.com>
2023-05-02 12:18:57 +0200

    Added Actividad18_Aplicacion - Unit 5

---
+++ Unit05/src/unit05/Actividad18_Aplicacion/Actividad18.java
@@ -0,0 +1,14 @@
+package unit05.Actividad18_Aplicacion;
+
+/*
+    En un juego de rol el mapa puede implementarse como una matriz donde las
+    filas y las columnas representan lugares (lugar 0, lugar 1, lugar 2, etc.) que estarán conectados.
+    Si desde el lugar X podemos ir hacia el lugar Y, entonces la matriz en la posición (X](v] valdrá cierto; en caso contrario, valdrá falso.
+    Escribe una función que, dada una matriz que representa el mapa y dos sitios,
+    indique si es posible viajar desde el primer sitio al segundo (directamente o pasando por lugares intermedios).
+ */
+public class Actividad18 {
+    public static void main(String[] args) {
+
+    }
+}
