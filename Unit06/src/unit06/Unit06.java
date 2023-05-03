f42f46d3020332b0351eb05c69cf8bbb97a75b3a
Jose Maria <jmcamposdev@gmail.com>
2023-05-03 11:51:35 +0200

    Created Unit6 Project

---
+++ Unit06/src/unit06/Unit06.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unit06;
+
+/**
+ *
+ * @author josemaria
+ */
+public class Unit06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
