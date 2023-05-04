a21cde1e13ec85faca387cbf833f1262d677a261
Jose Maria <jmcamposdev@gmail.com>
2023-05-04 10:32:48 +0200

    Added Description of the Activity Cola

--- Unit07/src/unit07/Actividad22_Aplicacion/Cola.java
+++ Unit07/src/unit07/Actividad22_Aplicacion/Cola.java
@@ -1,8 +1,15 @@
 package unit07.Actividad22_Aplicacion;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
+/*
+    Una cola es otra estructura dinámica como la pila, donde los elementos, en vez de apilarse y desapilarse, se encolan y desencolan.
+    La diferencia con las pilas es que se desencola el primer elemento encolado, ya que así es como funcionan las colas del autobús o del cine.
+    El primero que llega es el primero que sale de la cola (vamos a suponer que nadie se cuela).
+    Por tanto, los elementos se encolan y desencolan en extremos opuestos de la estructura,
+    llamados primero (el que está primero y será el próximo en abandonar la cola) y último (el que llegó último).
+    Implementa la clase Cola donde los elementos Integer encolados se guardan en una tabla.
+ */
 public class Cola {
     private Integer[] elementos;
 
