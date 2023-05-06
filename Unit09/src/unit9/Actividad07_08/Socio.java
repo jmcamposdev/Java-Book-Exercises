dcb28f30065b21f0bff5cf2e8719c6504faf4dd7
Jose Maria <jmcamposdev@gmail.com>
2023-05-06 11:51:12 +0200

    Added Actividad 8 - Unit 9

--- Unit09/src/unit9/Actividad07_08/Socio.java
+++ Unit09/src/unit9/Actividad07_08/Socio.java
@@ -1,10 +1,14 @@
-package unit9.Actividad07;
+package unit9.Actividad07_08;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 /*
+    Actividad 7
     Implementar la interfaz Comparable en la clase Socio par que por defecto se ordenen según los números de identificación - id - crecientes
+
+    Actividad 8
+    Cambiar la implementación de la clase Socio para que su criterio de ordenación natural sea por orden alfabético creciente de nombre
  */
 public class Socio implements Comparable<Socio>{
     private int id;
@@ -21,9 +25,15 @@ public class Socio implements Comparable<Socio>{
     public int edad(){
         return LocalDate.now().getYear() - fechaNacimiento.getYear();
     }
+//    Actividad 7
+//    @Override
+//    public int compareTo(Socio o) {
+//        return this.id - o.id;
+//    }
 
+    // Actividad 8
     @Override
     public int compareTo(Socio o) {
-        return this.id - o.id;
+        return this.nombre.compareTo(o.nombre);
     }
 }
