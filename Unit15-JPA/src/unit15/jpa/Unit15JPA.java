841c7da56578d2f3199b79b54f43d4eedcb5f240
Jose Maria <jmcamposdev@gmail.com>
2023-05-13 20:53:32 +0200

    Created Unit 15 JPA

---
+++ Unit15-JPA/src/unit15/jpa/Unit15JPA.java
@@ -0,0 +1,15 @@
+package unit15.jpa;
+
+/**
+ *
+ * @author josemaria
+ */
+public class Unit15JPA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
